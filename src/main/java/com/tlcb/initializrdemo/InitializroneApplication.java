539147495a8b76af17fd7f739c52df827b80eceb
mengxl <759498736@qq.com>
2017-04-22 10:16:10 +0800

    init

---
+++ src/main/java/com/tlcb/initializrdemo/InitializroneApplication.java
@@ -0,0 +1,12 @@
+package com.tlcb.initializrdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializroneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitializroneApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tlcb/initializrdemo/InitializroneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tlcb.initializrdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InitializroneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
